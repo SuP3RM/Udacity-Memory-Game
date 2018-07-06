cb304fd8e6fffed1d100a9025ea2c597b8c31421
Mario Aranda <mario.aranda2551@gmail.com>
2018-07-06 14:59:10 -0500

    First Commit
    Starting code

---
+++ js/app.js
@@ -0,0 +1,61 @@
+/*
+ * Create a list that holds all of your cards + variables
+ */
+
+let deck = document.querySelector(".deck");
+let allCards = deck.querySelectorAll("li.card");
+
+let restart = document.getElementById('restart-btn');
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+let shuffle = function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+//Display card when clicked
+ allCards.forEach(function(card){
+   card.addEventListener('click', function(){
+     card.classList.add('open', 'show');
+   });
+ });
+
+function resetGame() {
+  //face down allCards clicked
+  allCards.forEach(function(card){
+    card.classList.remove('open', 'show');
+  });
+
+}
+
+//shuffle cards within restarting game
+
+
+ restart.addEventListener('click', resetGame);
